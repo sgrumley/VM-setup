d3444040cc58155b911737be0fb848053fda1423
Sam Grumley <grumsamu@gmail.com>
2023-10-07 14:33:14 +1100

    added: go bin path to env path

--- golang.sh
+++ golang.sh
@@ -1,7 +1,10 @@
-# move to snap
+# apt
 sudo apt update
 sudo apt upgrade
 sudo apt install golang-go
 
+# snap
+sudo snap install go --classic
+
 # Go env settings
-sudo echo 'export PATH=$PATH:/usr/go/bin' | sudo tee -a /etc/profile
+sudo echo 'export PATH=$PATH:/usr/go/bin' | sudo tee -a ~/bashrc
